1bf59fc8b73324e990bc5942faa7e38b9be8d07e
Laurens Van Daele <58003049+LaurensVanDaele06@users.noreply.github.com>
2025-04-18 23:09:27 +0200

    Laurens eerste versie

--- CheddarChase/Game1.cs
+++ CheddarChase/Game1.cs
@@ -13,24 +13,31 @@ namespace CheddarChase
 
         public SpriteFont Font { get; set; }
         public Dictionary<string, Texture2D> Assets { get; set; }
+        public Texture2D Background { get; set; }
+        public Vector2 BackgroundPosition { get; set; } = new Vector2(0, 0);
+        public int Levens { get; set; }
 
         private AbstractState CurrentState;
+
 
         public Game1()
         {
             Graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            Levens = 0;
+
         }
 
-        public void ChangeState(AbstractState newState) {
+        public void ChangeState(AbstractState newState)
+        {
             CurrentState = newState;
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            const int width = 1280;
+            const int width = 1080;
             const int height = 720;
 
             Graphics.PreferredBackBufferWidth = width;
@@ -45,9 +52,9 @@ namespace CheddarChase
         protected override void LoadContent()
         {
             SpriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
             Assets = new Dictionary<string, Texture2D>();
+            Background = Content.Load<Texture2D>("background");
+            Font = Content.Load<SpriteFont>("GameFont");
         }
 
         protected override void Update(GameTime gameTime)
@@ -68,7 +75,8 @@ namespace CheddarChase
 
             // TODO: Add your drawing code here
             SpriteBatch.Begin();
-            CurrentState.Draw(gameTime);
+            SpriteBatch.Draw(Background, BackgroundPosition, Color.White);
+            SpriteBatch.DrawString(Font, "Aantal kaasjes: " + Levens, new Vector2(10, 10), Color.White);
             SpriteBatch.End();
 
             base.Draw(gameTime);
--- CheddarChase/States/PlayingState.cs
+++ CheddarChase/States/PlayingState.cs
@@ -7,26 +7,42 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework;
 
-namespace CheddarChase.States {
-    public class PlayingState : AbstractState {
+namespace CheddarChase.States
+{
+    public class PlayingState : AbstractState
+    {
         public int Levens { get; set; }
+        public Texture2D Background { get; set; }
+        public Vector2 BackgroundPosition { get; set; } = new Vector2(0, -700);
+        SpriteBatch SpriteBatch { get; set; }
 
-        public PlayingState(Game1 game) : base(game) {
-            Levens = 3;           
+
+
+        public PlayingState(Game1 game) : base(game)
+        {
+            Levens = 3;
+            Background = game.Assets["background"];
         }
 
-        public override void Draw(GameTime gameTime) {
-           
+        public override void Draw(GameTime gameTime)
+        {
+            SpriteBatch.Begin();
+            SpriteBatch.Draw(Background, BackgroundPosition, Color.White);
+            SpriteBatch.DrawString(game.Font, "Levens: " + Levens, new Vector2(10, 10), Color.White);
+            SpriteBatch.End();
         }
 
-        public override void Update(GameTime gameTime) {
-           
+        public override void Update(GameTime gameTime)
+        {
 
-            if (Levens < 1) {
+
+            if (Levens < 1)
+            {
                 game.ChangeState(new GameOverState(game));
             }
 
-            if (Keyboard.GetState().IsKeyDown(Keys.P)) {
+            if (Keyboard.GetState().IsKeyDown(Keys.P))
+            {
                 game.ChangeState(new PauzedState(game, this));
             }
         }
